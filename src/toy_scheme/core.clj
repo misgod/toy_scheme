712cc86bfd22570d8799e9b034921e8713db1447
misgod <misgod.tw@gmail.com>
2019-05-04 11:24:53 +0800

    working on macro

--- src/toy_scheme/core.clj
+++ src/toy_scheme/core.clj
@@ -1,49 +1,42 @@
 (ns toy-scheme.core
+  (:refer-clojure :exclude [macroexpand])
   (:require [clojure.tools.trace :refer :all]
             [clojure.test :as t]
             [instaparse.core :as insta]
             [sc.api]))
 
 
-(declare eval-seq scan-var form-apply extend-env setup-env eval-define eval-if eval-cond)
+(declare eval-seq scan-var lookup-var form-apply extend-env setup-env 
+         eval-define eval-lambda eval-if eval-defmacro eval-cond eval-assignment eval-let*)
 
 (defn form-eval [exp env]
   ;;(prn "eval" exp "===>\n" (keys (first (persistent! (env)))) )
-  (letfn [(constant? [x]
-            (or (number? x) (string? x) (nil? x) (instance? Boolean x)))
-          (make-proc [param body env ]
-            (list 'procedure  param body env))
-          (lookup-var [exp env]
-            (scan-var exp
-                      env
-                      #(get (deref %) exp)
-                      #(throw (Exception. "wrong binding id!")) ))
+  (letfn [(constant? [x] (or (number? x)
+                             (string? x)
+                             (nil? x)
+                             (instance? Boolean x)))
+          (lookup-var [exp env] (scan-var exp
+                                          env
+                                          #(get (deref %) exp)))]
 
-          (eval-assignment [exp env]
-            (let [var (second exp)
-                  val (nth exp 2)]
-              (scan-var var
-                        env
-                        #(swap! % assoc var (form-eval val env))
-                        #(throw (Exception. "wrong binding id!")))))]
-
-    (cond (constant? exp) exp
-          (symbol? exp) (lookup-var exp env)
-          (= (first exp) 'quote) (second exp)
-          (= (first exp) 'if) (eval-if exp env)
-          (= (first exp) 'cond) (eval-cond exp env)
+    (cond (constant? exp)         exp
+          (symbol? exp)           (lookup-var exp env)
+          (= (first exp) 'quote)  (second exp)
+          (= (first exp) 'if)     (eval-if exp env)
+          (= (first exp) 'cond)   (eval-cond exp env)
           (= (first exp) 'define) (eval-define exp env)
-          (= (first exp) 'begin) (eval-seq (rest exp) env)
-          (= (first exp) 'set!) (eval-assignment exp env)
-          (= (first exp) 'lambda) (make-proc (second exp)
-                                             (drop 2 exp)
-                                             env)
+          (= (first exp) 'begin)  (eval-seq (rest exp) env)
+          (= (first exp) 'set!)   (eval-assignment exp env)
+          (= (first exp) 'let*)   (eval-let* exp env)
+          (= (first exp) 'lambda) (eval-lambda exp env)
+          (= (first exp) 'defmacro) (eval-defmacro exp env)
+          (= (first exp) 'macroexpand) (macroexpand (second exp) env)
           :else (form-apply (form-eval (first exp) env)
                             (map #(form-eval % env) (rest exp))))))
 
 
 (defn form-apply [proc args]
-  ;;(prn "apply:" (take 3 proc) "=>" args)
+  ;(prn "apply:" (take 10 proc) "=>" args)
   (letfn [(primitive? [p] (= (first p) 'primitive))
           (apply-primitive [p a] (apply (second p) a)) ;;<-- magic
           (compound? [p] (= (first p) 'procedure))
@@ -55,8 +48,6 @@
                                       (extend-env (proc-env proc)
                                                   (proc-params proc)
                                                   args)))))
-
-
 
 (def primitive-procs {
                       'true true
@@ -74,13 +65,15 @@
                       'reduce reduce
                       'filter filter
                       'list list
+                      'cons cons
                       })
 (defn setup-env []
   (-> '()
       (extend-env (keys primitive-procs)
                   (map #(list 'primitive %) (vals primitive-procs)))
       ;;above is base env
-      (extend-env '() '())))
+      (extend-env '() '()))) ;;avoid to modify base env
+
 
 (defn define-var [exp]
   (let [var (second exp)]
@@ -95,12 +88,27 @@
       (form-eval (make-lambda (rest var) (drop 2 exp)) env)
       (form-eval (nth exp 2) env))))
 
+
 (defn eval-define [exp env]
   (let [var (define-var exp)
         val (define-val exp env)
         e (first env)]
-    (swap! e assoc var val)
-    "ok"))
+    (swap! e assoc var val)))
+
+
+(defn eval-assignment [exp env]
+  (let [var (second exp)
+        val (nth exp 2)]
+    (scan-var var
+              env
+              #(swap! % assoc var (form-eval val env)))))
+
+(defn eval-lambda [exp env]
+  (list 'procedure
+        (second exp)
+        (drop 2 exp)
+        env))
+
 
 (defn eval-if [exp env]
   (if (form-eval (second exp) env)
@@ -109,16 +117,16 @@
 
 (defn eval-cond [exp env]
  ; (let [(rest exp)])
-
-
+  
   )
+
 
 
 (defn scan-var [var env action not-found]
   (let [e (first env)]
-    (cond (nil? e) (not-found)
+    (cond (nil? e)           (throw (Exception. "wrong binding id!"))
           (contains? @e var) (action e)
-          :else (recur var (rest env) action not-found))))
+          :else              (recur var (rest env) action))))
 
 (defn eval-seq [exps env]
   (reduce #(form-eval %2 env) nil exps))
@@ -130,49 +138,40 @@
     (let [x (first vr)
           y (first vl)]
       (cond
-        (and (nil? x) (nil? y)) (cons (atom vmap) env)
-        (or (nil? x) (nil? y)) (throw (Exception. "arguments not match!" env))
-        (= x '.) (recur nil nil  (assoc vmap (second vr) vl))
-        :else (recur (rest vr) (rest vl) (assoc vmap x y))))))
-
-(defn -main
-  "I don't do a whole lot ... yet."
-  [& args]
-  (println "Hello, World!"))
+        (and (nil? x) (nil? y))  (cons (atom vmap) env)
+        (or (nil? x) (nil? y))   (throw (Exception. "arguments not match!" env))
+        (= x '.)                 (recur nil nil (assoc vmap (second vr) vl))
+        :else                    (recur (rest vr) (rest vl) (assoc vmap x y))))))
 
 
+(defn eval-let* [exp env]
+  (let [eenv (extend-env env '() '())
+        vars (second exp)]
+    (doseq [[b e] vars]
+      (swap! (first eenv) assoc b (form-eval e eenv)))
+    (form-eval (nth exp 2) eenv)))
 
-(t/is (= 34 (form-eval '(- (+ (* 2 3 5) 1 6) (+ 1 2)) (setup-env))))
-(t/is (= 2 (form-eval '(if false 1 2) (setup-env))))
-(t/is (= 1 (form-eval '(if true 1 2) (setup-env))))
+(defn eval-defmacro [exp env]
+  (let [[a1 a2 a3] env
+        mbody      (with-meta (form-eval a3 env) {:ismacro true})]
+    (swap! (first env) assoc a2 mbody)))
 
-(t/is (= 'a (form-eval '(quote a) (setup-env))))
+(defn is-macro-call [exp env]
+  (and (seq? exp)
+       (symbol? (first exp))
+       (env/env-find env (first ast))
+       (:ismacro (meta (env/env-get env (first ast))))))
 
-(t/is (= 11 (form-eval '((lambda (a b) (+ a b)) 6 5) (setup-env))))
-(t/is (= 22 (form-eval '((lambda (x) (* 2 x)) ((lambda (a b) (+ a b)) 6 5)) (setup-env))))
+(defn macroexpand [exp env]
+  (loop [exp ast]
+    (if (is-macro-call ast env)
+      (let [mac (env/env-get env (first ast))]
+        (recur (apply mac (rest ast))))
+      ast)))
 
-(t/is (= 15 (form-eval '((lambda (a b . c) (+ a  b (count c))) 6 5 4 3 2 1) (setup-env))))
-
-(t/is (= 6 (form-eval '((lambda (x) (begin (set! x  (+ x 1)) x)) 5) (setup-env))))
 
 
-(t/is (= 6 (form-eval '(begin
-                       (define x 5)
-                       (define x (+ x 1))
-                       (lambda () (define x 10))
-                       x) (setup-env))))
-(t/is (= 15 (form-eval '(begin
-                         (define (f x y) (* x y))
-                         (f 3 5))
-                       (setup-env))))
-
-(t/is (= 120 (form-eval '(begin
-                          (define (f x y)
-                            (if (eq? x 1)
-                              y
-                              (f (- x 1) (* x y))))
-                          (f 5 1))
-                        (setup-env))))
+;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
 
 
 
@@ -199,3 +198,12 @@
           :else (do
                   (prn  (form-eval (read-string form) global-env))
                   (recur "")))))))
+
+
+(defn -main  [& args] (repl))
+
+
+(define-macro when
+  (lambda (test . branch)
+          (list 'if test
+                (cons 'begin branch))))
--- test/toy_scheme/core_test.clj
+++ test/toy_scheme/core_test.clj
@@ -2,6 +2,52 @@
   (:require [clojure.test :refer :all]
             [toy-scheme.core :refer :all]))
 
-(deftest a-test
-  (testing "FIXME, I fail."
-    (is (= 0 1))))
+(def feval #(form-eval % (setup-env)))
+
+
+(deftest my-test
+  (testing "xxxx"
+    
+    (is (= 34 (feval '(- (+ (* 2 3 5) 1 6) (+ 1 2)))))
+    (is (= 2  (feval '(if false 1 2))))
+    (is (= 1  (feval '(if true 1 2))))
+    (is (= 'a (feval '(quote a))))
+    (is (= 11 (feval '((lambda (a b) (+ a b)) 6 5))))
+    (is (= 22 (feval '((lambda (x) (* 2 x)) ((lambda (a b) (+ a b)) 6 5)))))
+    
+    (is (= 15 (feval '((lambda (a b . c) (+ a  b (count c))) 6 5 4 3 2 1))))
+    (is (= 6  (feval '((lambda (x) (begin (set! x  (+ x 1)) x)) 5))))
+    
+    
+    (is (= 6  (feval '(begin
+                         (define x 5)
+                         (define x (+ x 1))
+                         (lambda () (define x 10))
+                         x))))
+    
+    (is (= 15 (feval '(begin
+                         (define (f x y) (* x y))
+                         (f 3 5)))))
+    
+    ;;recursion
+    (is (= 720 (feval '(begin
+                          (define (fact x)
+                            (if (eq? x 1)
+                              1
+                              (* x (fact (- x 1)))))
+                          (fact 6)))))
+    
+    ;;closure
+    (is (= 15 (feval '(begin
+                         (define (f y)
+                           (lambda (x) (+ x y)))
+                         ((f 10) 5)))))
+    
+    
+    
+    (is (= 10 (feval '(let* ((x 3)
+                             (y (+ x 4)))
+                             (+ y x)))))))
+  
+
+
